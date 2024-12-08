1c40dcbe0af6274bf3acfd5caf9c019ed16b8dbf
Beyza Kesler <beyzakslr@hotmail.com>
2024-12-08 14:26:15 +0300

    ..

---
+++ C#EgitimKampi301.PresentationLayer/FrmCategory.cs
@@ -0,0 +1,256 @@
+﻿using C_EgitimKampi301.BusinessLayer.Abstract;
+using C_EgitimKampi301.BusinessLayer.Concrete;
+using C_EgitimKampi301.DataAccessLayer.EntityFramework;
+using C_EgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_EgitimKampi301.PresentationLayer
+{
+    public partial class FrmCategory : Form
+    {
+        private Label label1;
+        private Button btnList;
+        private DataGridView dataGridView1;
+        private TextBox txtCategoryId;
+        private TextBox txtCategoryName;
+        private Label label2;
+        private Label label3;
+        private RadioButton rdbActive;
+        private RadioButton rdbPassive;
+        private Button btnAdd;
+        private Button btnGetById;
+        private Button btnUpdate;
+        private Button btnDelete;
+
+       
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCategoryId = new System.Windows.Forms.TextBox();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdbActive = new System.Windows.Forms.RadioButton();
+            this.rdbPassive = new System.Windows.Forms.RadioButton();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kategory ID:";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(30, 142);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(131, 31);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(314, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(275, 181);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // txtCategoryId
+            // 
+            this.txtCategoryId.Location = new System.Drawing.Point(113, 22);
+            this.txtCategoryId.Name = "txtCategoryId";
+            this.txtCategoryId.Size = new System.Drawing.Size(100, 22);
+            this.txtCategoryId.TabIndex = 3;
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Location = new System.Drawing.Point(113, 60);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(100, 22);
+            this.txtCategoryName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kategory Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kategory Durum:";
+            // 
+            // rdbActive
+            // 
+            this.rdbActive.AutoSize = true;
+            this.rdbActive.Location = new System.Drawing.Point(139, 103);
+            this.rdbActive.Name = "rdbActive";
+            this.rdbActive.Size = new System.Drawing.Size(53, 20);
+            this.rdbActive.TabIndex = 8;
+            this.rdbActive.TabStop = true;
+            this.rdbActive.Text = "Aktif";
+            this.rdbActive.UseVisualStyleBackColor = true;
+            // 
+            // rdbPassive
+            // 
+            this.rdbPassive.AutoSize = true;
+            this.rdbPassive.Location = new System.Drawing.Point(216, 101);
+            this.rdbPassive.Name = "rdbPassive";
+            this.rdbPassive.Size = new System.Drawing.Size(58, 20);
+            this.rdbPassive.TabIndex = 9;
+            this.rdbPassive.TabStop = true;
+            this.rdbPassive.Text = "Pasif";
+            this.rdbPassive.UseVisualStyleBackColor = true;
+            this.rdbPassive.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(32, 171);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(131, 31);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.Location = new System.Drawing.Point(32, 258);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(131, 31);
+            this.btnGetById.TabIndex = 11;
+            this.btnGetById.Text = "ID\'ye göre getir";
+            this.btnGetById.UseVisualStyleBackColor = true;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(32, 229);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(131, 31);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(32, 200);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(131, 31);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // FrmCategory
+            // 
+            this.ClientSize = new System.Drawing.Size(601, 390);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.rdbPassive);
+            this.Controls.Add(this.rdbActive);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCategoryName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCategoryId);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCategory";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private readonly ICategoryService _categoryService;
+
+        public FrmCategory()
+        {
+            _categoryService = new CategoryManager(new EfCategoryDal());
+            InitializeComponent();
+        }
+
+     
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName = txtCategoryName.Text;
+            category.CategoryStatus = true;
+            _categoryService.TInsert(category);
+            MessageBox.Show("Ekleme Başarılı");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryValues = _categoryService.TGetAll();
+            dataGridView1.DataSource = categoryValues;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id= int.Parse(txtCategoryId.Text);
+            var deletedValues = _categoryService.TGetById(id);
+            _categoryService.TDelete(deletedValues);
+            MessageBox.Show("Silme Başarılı");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtCategoryId.Text);
+            var values = _categoryService.TGetById(id);
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            
+            int updatedId = int.Parse(txtCategoryId.Text);
+            var updatedValue = _categoryService.TGetById(updatedId);
+            updatedValue.CategoryName=txtCategoryName.Text;
+            updatedValue.CategoryStatus = true;
+            _categoryService.TUpdate(updatedValue);
+            MessageBox.Show("Güncelleme Başarılı");
+        }
+    }
+}
